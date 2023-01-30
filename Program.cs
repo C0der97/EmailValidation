8d5ad0cc13c422c029d99f9be069c1f49bd02fde
Angel Bareno <angel.bareno@softwareone.com>
2023-01-30 16:04:18 -0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+                namespace HelloWorld
+{    
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var watch = new System.Diagnostics.Stopwatch();
+        string chars= "^|<>%&#$-+/*'!?{}=^~:;%()[]";
+        var caracters = chars.AsEnumerable();
+        string mail = "patrickSon_@oubluk.com";
+        bool containsSpecialCharacters = false;
+
+        foreach (char item in chars.AsEnumerable())
+        {
+            containsSpecialCharacters = mail.Contains(item);
+            if(containsSpecialCharacters){
+              break;
+            }
+        }
+
+        bool valid = Regex.IsMatch(mail,"^\\S+@\\S+\\.\\S+$",RegexOptions.IgnoreCase,TimeSpan.FromMilliseconds(250)) && !containsSpecialCharacters && mail.Length <= 50;
+        Console.WriteLine("Is valid mail "+valid);
+        watch.Stop();
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+}
+}
